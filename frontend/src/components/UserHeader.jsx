c0c70599255da7b7b8153e0374319e24fdb08039
joule18 <jrjundam18@gmail.com>
2024-08-19 11:06:09 +0800

    dynamic user header page

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -11,7 +11,7 @@ import {
   useToast,
 } from "@chakra-ui/react";
 
-const UserHeader = () => {
+const UserHeader = ({ user }) => {
   const toast = useToast();
   const copyUrl = () => {
     const currentUrl = window.location.href;
@@ -30,10 +30,10 @@ const UserHeader = () => {
       <Flex justifyContent={"space-between"} w={"full"}>
         <Box>
           <Text fontSize={"2xl"} fontWeight={"bold"}>
-            Mark Zuckerberg
+            {user.name}
           </Text>
           <Flex gap={2} alignItems={"center"}>
-            <Text fontSize={"sm"}>markzuckerberg</Text>
+            <Text fontSize={"sm"}>{user.username}</Text>
             <Text
               fontSize={"xs"}
               bg={"gray.dark"}
@@ -46,20 +46,31 @@ const UserHeader = () => {
           </Flex>
         </Box>
         <Box>
-          <Avatar
-            name="Mark Zuckerberg"
-            src="/zuck-avatar.png"
-            size={{
-              base: "md",
-              md: "xl",
-            }}
-          />
+          {user.profilePic ? (
+            <Avatar
+              name={user.name}
+              src={user.profilePic}
+              size={{
+                base: "md",
+                md: "xl",
+              }}
+            />
+          ) : (
+            <Avatar
+              name={user.name}
+              src="https://bit.ly/broken-link"
+              size={{
+                base: "md",
+                md: "xl",
+              }}
+            />
+          )}
         </Box>
       </Flex>
-      <Text>Co-founder, executive chairman and CEO of Meta Platforms.</Text>
+      <Text>{user.bio}</Text>
       <Flex w={"full"} justifyContent={"space-between"}>
         <Flex gap={2} alignItems={"center"}>
-          <Text color={"gray.light"}>3.2k followers</Text>
+          <Text color={"gray.light"}>{user.followers.length}</Text>
           <Box w={1} h={1} bg={"gray.light"} borderRadius={"full"}></Box>
           <Link color={"gray.light"}>instagram.com</Link>
         </Flex>
--- frontend/src/hooks/useShowToast.js
+++ frontend/src/hooks/useShowToast.js
@@ -1,16 +1,20 @@
 import { useToast } from "@chakra-ui/react";
+import { useCallback } from "react";
 
 const useShowToast = () => {
   const toast = useToast();
-  const showToast = (title, description, status) => {
-    toast({
-      title,
-      description,
-      status,
-      duration: 3000,
-      closeable: true,
-    });
-  };
+  const showToast = useCallback(
+    (title, description, status) => {
+      toast({
+        title,
+        description,
+        status,
+        duration: 3000,
+        closeable: true,
+      });
+    },
+    [toast]
+  );
   return showToast;
 };
 export default useShowToast;
