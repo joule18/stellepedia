9e4d5ef966664342a208a306ff2faad88392b2e0
joule18 <jrjundam18@gmail.com>
2024-08-23 17:51:25 +0800

    complete getSuggestedUsers api endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -232,6 +232,37 @@ const updateUser = async (req, res) => {
   }
 };
 
+const getSuggestedUsers = async (req, res) => {
+  try {
+    //exclude the current user from suggested users array, and other users already being followed
+
+    const userId = req.user._id;
+    const usersFollowedByYou = await User.findById(userId).select("following");
+
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId },
+        },
+      },
+      {
+        $sample: { size: 10 },
+      },
+    ]);
+    const filteredUsers = users.filter(
+      (user) => !usersFollowedByYou.following.includes(user._id.toString())
+    );
+    const suggestedUsers = filteredUsers.slice(0, 4);
+
+    suggestedUsers.forEach((user) => (user.password = null));
+
+    res.status(200).json(suggestedUsers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in getSuggestedUsers ", error.message);
+  }
+};
+
 export {
   signupUser,
   loginUser,
@@ -239,4 +270,5 @@ export {
   followUnfollowUser,
   updateUser,
   getUserProfile,
+  getSuggestedUsers,
 };
